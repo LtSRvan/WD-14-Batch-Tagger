8ac508d7df9b60b1a0a30d5359ad00060a3ca6a9
LtSRvan <dylansantizo47@gmail.com>
2023-03-30 08:23:47 -0600

    Create a venv for the script

---
+++ create-venv.bat
@@ -0,0 +1,14 @@
+@echo off
+
+python -m venv WD-14-Tagger
+
+echo Virtual environment created successfully.
+echo.
+echo To activate the environment, run the following command (run this every time you want to use the script):
+echo.
+echo     activate WD-14-Tagger
+echo.
+echo If that doesn't work try
+echo.
+echo     WD-14-Tagger\Scripts\activate.bat
+pause
